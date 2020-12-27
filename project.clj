750e7863123dac5d14004c843184c2e6ad0b46a9
Anne-Mari Silvast <annemari.silvast@gmail.com>
2020-12-27 19:17:51 +0200

    corrected dependencies

--- project.clj
+++ project.clj
@@ -5,9 +5,12 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [clj-http "3.10.3"]
+                 [org.clojure/tools.logging "1.1.0"]
+                 [org.clojure/data.json "1.0.0"]
                  [com.cognitect.aws/api "0.8.484"]
                  [com.cognitect.aws/s3 "810.2.801.0"]
                  [com.cognitect.aws/sqs "809.2.784.0"]
+                 [com.cognitect.aws/dynamodb "809.2.784.0"]
                  [com.cognitect.aws/endpoints "1.1.11.914"]]
   :main ^:skip-aot aanestysbot.core
   :target-path "target/%s"
