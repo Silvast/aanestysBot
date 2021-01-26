7301ef0db33ecd2f31e66f72971d5d9855b8cca7
Anne-Mari Silvast <anne-mari.silvast@yle.fi>
2021-01-26 10:19:16 +0200

    fixed error with data formatting

--- src/aanestysbot/edustajat.clj
+++ src/aanestysbot/edustajat.clj
@@ -8,13 +8,19 @@
   (str "https://avoindata.eduskunta.fi/api/v1/tables/SaliDBAanestysEdustaja/rows?columnName=AanestysId&columnValue=" id "&page=" page))
 
 (defn create-tweetable-data
-  [data kohta]
+  [data]
   (let [keys (map keyword (:columnNames data))
         formatted-data (map #(zipmap keys %) (:rowData data))]
     formatted-data))
 
+(defn trunc [s n]
+  (let [minified-string (subs s 0 (min (count s) n))]
+    (if (< (count minified-string) (count s))
+      (str minified-string "..")
+      s)))
+
 (defn form-better-voting-url [url]
-  (let [numbers (-> url (clojure.string/split #"\/"))]
+  (let [numbers (-> url (s/replace "https://www.eduskunta.fi/aanestystulos/" "") (s/split #"\/"))]
     (str "https://www.eduskunta.fi/FI/Vaski/sivut/aanestys.aspx?aanestysnro=" (first numbers) "&istuntonro=" (second numbers)
          "&vuosi=" (nth numbers 2))))
 
@@ -24,8 +30,8 @@
         mp (rand-int 200)
         data-1 (json/read-json (:body (client/get url-1)))
         data-2 (json/read-json (:body (client/get url-2)))
-        all-data (concat (create-tweetable-data data-1 kohta) (create-tweetable-data data-2 kohta))
+        all-data (concat (create-tweetable-data data-1) (create-tweetable-data data-2))
         final-data (conj (nth all-data mp) {:kohta kohta} {:pk pk})]
-    (format "Edustaja %s %s (%s) äänesti %s, äänestyksessä: %s - ks. %s" (:EdustajaEtunimi final-data) (:EdustajaSukunimi final-data) (s/trim (:EdustajaRyhmaLyhenne final-data)) (s/trim (:EdustajaAanestys final-data)) (:kohta final-data) (form-better-voting-url (:pk final-data)))))
+    (format "Edustaja %s %s (%s) äänesti %s, äänestyksessä: %s - ks. %s" (:EdustajaEtunimi final-data) (:EdustajaSukunimi final-data) (s/trim (:EdustajaRyhmaLyhenne final-data)) (s/trim (:EdustajaAanestys final-data)) (trunc (:kohta final-data) 110) (form-better-voting-url (:pk final-data)))))
 
-;; (get-random-mp-vote 45511 "Hallituksen esitys eduskunnalle laiksi varhaiskasvatuksen asiakasmaksuista annetun lain 5 ja 8 §:n muuttamisesta" "https://www.eduskunta.fi/FI/Vaski/sivut/aanestys.aspx?aanestysnro=23&istuntonro=158&vuosi=2020")
+;; (def testaus (get-random-mp-vote 45511 "Hallituksen esitys eduskunnalle laiksi varhaiskasvatuksen asiakasmaksuista annetun lain 5 ja 8 §:n muuttamisesta muuttamisesta muuttamisesta" "https://www.eduskunta.fi/aanestystulos/155/161/2020"))
